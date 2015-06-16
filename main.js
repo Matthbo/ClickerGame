a0e9c36d2aaa018f12e46226c7cd98617de67469
Matthijs <matthijs.booman@hotmail.com>
2015-06-17 00:24:03 +0200

    simple stuff

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function init(){
-    
+var playerHealth;
+var playerArmor;
+var playerClickDamage;
+var playerAutoDamage;
+var level;
+
+function preInit(){
+    playerHealth = 100;
+    playerArmor = 0;
+    playerClickDamage = 10;
+    playerAutoDamage = 0;
+    level = 0;
 }
 
+function init(){
+    //load database stuff here (if it exists for this player)
+    //todo: start the game, game logic, enemies, all the things!
+}
+
+preInit();
 init();
